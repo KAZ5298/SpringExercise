3c260f4088ff53654f3bcf4edbe0f6e8d8c85b84
KAZUHIKO <kazuhiko.d@gmail.com>
2024-07-17 15:45:04 +0900

    EX2. 画面遷移 完了

--- src/main/java/katachi/spring/exercise/hello/HelloController.java
+++ src/main/java/katachi/spring/exercise/hello/HelloController.java
@@ -2,6 +2,7 @@ package katachi.spring.exercise.hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HelloController {
@@ -9,5 +10,16 @@ public class HelloController {
 	@GetMapping("/index")
 	public String getHello() {
 		return "/";
+	}
+	
+	// EX2.
+	@GetMapping("/input")
+	public String getInput() {
+		return "/input";
+	}
+	
+	@PostMapping("/index")
+	public String postRequest() {
+		return "/input";
 	}
 }
