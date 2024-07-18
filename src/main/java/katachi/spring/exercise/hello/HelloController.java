2106ff6ad2339fa94ff1aa2a95036a7b809c5876
KAZUHIKO <kazuhiko.d@gmail.com>
2024-07-18 15:02:47 +0900

    EX3. GETパラメータ 完了

--- src/main/java/katachi/spring/exercise/hello/HelloController.java
+++ src/main/java/katachi/spring/exercise/hello/HelloController.java
@@ -1,25 +1,31 @@
 package katachi.spring.exercise.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 	
+	// EX1.
 	@GetMapping("/index")
 	public String getHello() {
 		return "/";
 	}
 	
 	// EX2.
-	@GetMapping("/input")
-	public String getInput() {
-		return "/input";
-	}
+//	@GetMapping("/input")
+//	public String getInput() {
+//		return "/input";
+//	}
 	
-	@PostMapping("/index")
-	public String postRequest() {
+	// EX3.
+	@GetMapping("/input")
+	public String getInput(@RequestParam("name") String str, Model model) {
+		
+		model.addAttribute("name", str);
+		
 		return "/input";
 	}
 }
