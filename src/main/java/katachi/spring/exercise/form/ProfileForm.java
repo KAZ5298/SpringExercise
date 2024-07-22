fe6ffe21abde8b4c87be728c4ccdd03b64749b90
KAZUHIKO <kazuhiko.d@gmail.com>
2024-07-22 15:35:47 +0900

    EX5. フォーム 未完 2024/07/22

---
+++ src/main/java/katachi/spring/exercise/form/ProfileForm.java
@@ -0,0 +1,24 @@
+package katachi.spring.exercise.form;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class ProfileForm {
+	
+	private String name;
+
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date birthday;
+	
+	private Integer gender;
+	
+	private Integer bloodtype;
+	
+	private Integer study;
+	
+	private String remarks;
+}
--- src/main/java/katachi/spring/exercise/hello/HelloController.java
+++ src/main/java/katachi/spring/exercise/hello/HelloController.java
@@ -1,12 +1,23 @@
 package katachi.spring.exercise.hello;
 
+
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import katachi.spring.exercise.application.service.UserApplicationService;
+import katachi.spring.exercise.form.ProfileForm;
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class HelloController {
 	
 	// EX1.
@@ -22,13 +33,13 @@ public class HelloController {
 //	}
 	
 	// EX3.
-	@GetMapping("/input")
-	public String getInput(@RequestParam(name = "name", required = false) String str, Model model) {
-		
-		model.addAttribute("name", str);
-		
-		return "/input";
-	}
+//	@GetMapping("/input")
+//	public String getInput(@RequestParam(name = "name", required = false) String str, Model model) {
+//		
+//		model.addAttribute("name", str);
+//		
+//		return "/input";
+//	}
 	
 	// EX4.
 	@PostMapping("/input")
@@ -38,5 +49,33 @@ public class HelloController {
 		
 		return "/profile";
 	}
-
+	
+	// EX5.
+	@Autowired
+	private UserApplicationService userApplicationService;
+	
+	@GetMapping("/input")
+	public String getInput(@RequestParam(name = "name", required = false) String str, Model model) {
+		
+		Map<String, Integer> genderMap = userApplicationService.getGenderMap();
+		model.addAttribute("genderMap", genderMap);
+		
+		Map<String, Integer> bloodtypeMap = userApplicationService.getBloodtypeMap();
+		model.addAttribute("bloodtypeMap", bloodtypeMap);
+		
+		Map<String, Integer> studyMap = userApplicationService.getStudyMap();
+		model.addAttribute("studyMap", studyMap);
+		
+		model.addAttribute("name", str);
+		
+		return "/input";
+	}
+	
+	@PostMapping("/profile")
+	public String requestProfile(@ModelAttribute ProfileForm form) {
+		
+		log.info(form.toString());
+		
+		return "/profile";
+	}
 }
