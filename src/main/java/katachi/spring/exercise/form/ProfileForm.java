8fb9dc11964b3e32a519455dcb9d600139f02480
KAZUHIKO <kazuhiko.d@gmail.com>
2024-07-30 14:53:29 +0900

    EX6. バリデーション 完了

--- src/main/java/katachi/spring/exercise/form/ProfileForm.java
+++ src/main/java/katachi/spring/exercise/form/ProfileForm.java
@@ -3,8 +3,8 @@ package katachi.spring.exercise.form;
 import java.util.Date;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
